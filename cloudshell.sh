c9076fd24eb49dc73583ae57e4c599578dc27a24
Samsul Hadi <samsulhadi@Samsuls-MacBook-Air-2.local>
2021-03-16 16:43:23 +0700

    init

--- cloudshell.sh
+++ cloudshell.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-
 # Task 1
-
 # git clone https://github.com/GoogleCloudPlatform/gke-tracing-demo
 cd gke-tracing-demo
 cd terraform
@@ -12,13 +9,16 @@ gcloud config set compute/zone us-central1-a
 
 PROJECT_ID=$(gcloud config get-value project)
 
+terraform init
+
 terraform plan -out=tfplan -var "project=$PROJECT_ID" -var "zone=us-central1-a"
 terraform apply tfplan 
 
 kubectl apply -f tracing-demo-deployment.yaml
 
-echo http://$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# echo http://$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-gcloud pubsub subscriptions pull --auto-ack --limit 10 tracing-demo-cli
+# gcloud pubsub subscriptions pull --auto-ack --limit 10 tracing-demo-cli
 
+# setelah dapet IP
 curl http://$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')?string=HelloWorld
